ad6cb06c6c151adc9298db9db8223ef6a6103ccb
Nathalie Redick <>
2020-10-03 14:41:04 -0400

    text dir

--- application/webapp/urls.py
+++ application/webapp/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('text/', include('text.urls')),
+    path('links/', include('links.urls')),
     path('admin/', admin.site.urls),
 ]
