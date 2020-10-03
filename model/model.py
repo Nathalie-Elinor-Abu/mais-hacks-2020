717490816aef325ee36989e789c2aaa30e74a414
elinorpd <59417117+elinorpd@users.noreply.github.com>
2020-10-03 18:05:26 -0400

    more reqs

--- model/model.py
+++ model/model.py
@@ -2,3 +2,7 @@ from transformers import DistilBertModel, DistilBertConfig
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+data = pd.read_csv("../data/clean_data.csv")
