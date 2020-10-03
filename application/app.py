1a7ecabff016db4350c50b5aa1e3ef591b92702b
Nathalie Redick <>
2020-10-03 16:14:47 -0400

    refractor

--- application/app.py
+++ application/app.py
@@ -1,5 +1,6 @@
 import flask
 import pickle
+
 # Use pickle to load in the pre-trained model.
 #todo with open(f'model/mbti_model.pkl', 'rb') as f:
 #    model = pickle.load(f)
@@ -8,23 +9,23 @@ app = flask.Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
-def main():
-    return flask.render_template('main_page.html')
-
-
-@app.route('/out/')
-def output():
-    return flask.render_template('output_page.html')
-
-
-@app.route('/about/')
-def about():
-    return flask.render_template('about_page.html')
-
-
-@app.route('/mbti-text/')
 def text():
-    return flask.render_template('about_page.html')
+    return flask.render_template('index.html')
+
+
+@app.route('/twitter-links/')
+def main():
+    return flask.render_template('links.html')
+
+
+@app.route('/cp-text/')
+def output():
+    return flask.render_template('cp-text.html')
+
+
+@app.route('/result/')
+def output():
+    return flask.render_template('result.html')
 
 
 if __name__ == '__main__':
