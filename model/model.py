e97c9581b4fa50eeee8d846da730b5ff7d39c512
elinorpd <59417117+elinorpd@users.noreply.github.com>
2020-10-03 13:39:54 -0400

    explode tweets into sep rows

--- model/model.py
+++ model/model.py
@@ -4,12 +4,25 @@ import os
 
 data = pd.read_csv("../data/mbti.csv")
 
-# printing the first 10 lines
-print(data.head(10))
-print(data.columns)
-print(data.shape) # 8765 rows, 2 columns
+# printing the first 5 lines
+print(data.head(5))
+print("Shape of the data", data.shape) # 8675 rows, 2 columns
 
 # finding all the personality types
 types = list(np.unique(data.type.values))
-print("There are", types.shape[0], "personality types")
-print(*types, sep='\n')
+types_dict = {key: i for i, key in enumerate(types)}
+print("There are", len(types), "personality types")
+print(*types, sep=', ')
+
+# assigning numbers to each type and putting it in the type_index column
+def get_type_index(typ):
+    return types_dict[typ]
+data['type_index']=data['type'].apply(get_type_index)
+
+# cleaning up and separating the tweets
+def sep_tweets(tweets):
+    return tweets.split('|||')
+data['tweets_list']=data['posts'].apply(sep_tweets)
+print(data.head())
+data_separated = data.explode('tweets_list', ignore_index=True)
+print(data_separated.shape)
