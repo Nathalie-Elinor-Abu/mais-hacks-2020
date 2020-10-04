319d1fec33b44ac831a41a4afa1114a961e35cf9
elinorpd <59417117+elinorpd@users.noreply.github.com>
2020-10-03 22:50:14 -0400

    working on LSTM model

--- model/model.py
+++ model/model.py
@@ -15,6 +15,8 @@ from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 MAX_SEQ_LEN = 25
 DEFAULT_BATCH_SIZE = 128
@@ -57,6 +59,8 @@ encoder = LabelEncoder()
 
 y_train = encoder.fit_transform(train['type'].values)
 y_train = to_categorical(y_train)
+y_val = encoder.fit_transform(val['type'].values)
+y_val = to_categorical(y_val)
 
 y_test = encoder.fit_transform(test['type'].values)
 y_test = to_categorical(y_test)
@@ -69,5 +73,107 @@ print('Distribution of Classes:', ctr)
 
 # get class weights for the training data, this will be used data
 y_train_int = np.argmax(y_train,axis=1)
-cws = class_weight.compute_class_weight('balanced', np.unique(y_train_int), y_train_int)
+cws = class_weight.compute_class_weight('balanced', classes=np.unique(y_train_int), y=y_train_int)
 print(cws)
+cws_dict = {i[0]:w for i,w in np.ndenumerate(cws)}
+print("class weights dictionary:\n",cws_dict)
+
+
+
+'''
+# BASELINE STATS AND TESTS
+print('Dominant Class: ', ctr.most_common(n = 1)[0][0])
+print('Baseline Accuracy Dominant Class', (ctr.most_common(n = 1)[0][0] == test['type'].values).mean())
+
+preds = np.zeros_like(y_test)
+preds[:, 0] = 1
+preds[0] = 1 #done to suppress warning from numpy for f1 score
+print('F1 Score:', f1_score(y_test, preds, average='weighted'))
+
+# Naive Bayse Baseline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultinomialNB()),
+])
+text_clf.fit(tokenizer.sequences_to_texts_generator(train_text_vec), y_train.argmax(axis=1))
+predictions = text_clf.predict(tokenizer.sequences_to_texts_generator(test_text_vec))
+print('Baseline Accuracy Using Naive Bayes: ', (predictions == y_test.argmax(axis = 1)).mean())
+print('F1 Score:', f1_score(y_test.argmax(axis = 1), predictions, average='weighted'))'''
+
+
+# LSTM MODEL
+def threshold_search(y_true, y_proba, average = None):
+    best_threshold = 0
+    best_score = 0
+    for threshold in [i * 0.01 for i in range(100)]:
+        score = f1_score(y_true=y_true, y_pred=y_proba > threshold, average=average)
+        if score > best_score:
+            best_threshold = threshold
+            best_score = score
+    search_result = {'threshold': best_threshold, 'f1': best_score}
+    return search_result
+
+
+def train(model,
+          X_train, y_train, X_test, y_test, X_val, y_val,
+          checkpoint_path='model.hdf5',
+          epcohs = 25,
+          batch_size = DEFAULT_BATCH_SIZE,
+          class_weights = None,
+          fit_verbose=2,
+          print_summary = True
+         ):
+    m = model()
+    if print_summary:
+        print(m.summary())
+    m.fit(
+        X_train,
+        y_train,
+        validation_data=(X_val, y_val),
+        epochs=epcohs,
+        batch_size=batch_size,
+        class_weight=class_weights,
+         #saves the most accurate model, usually you would save the one with the lowest loss
+        callbacks= [
+            ModelCheckpoint(checkpoint_path, monitor='val_acc', verbose=1, save_best_only=True),
+            EarlyStopping(patience = 2)
+        ],
+        verbose=fit_verbose
+    )
+    print("\n\n****************************\n\n")
+    print('Loading Best Model...')
+    m.load_weights(checkpoint_path)
+    predictions = m.predict(X_test, verbose=1)
+    print('Validation Loss:', log_loss(y_test, predictions))
+    print('Test Accuracy', (predictions.argmax(axis = 1) == y_test.argmax(axis = 1)).mean())
+    print('F1 Score:', f1_score(y_test.argmax(axis = 1), predictions.argmax(axis = 1), average='weighted'))
+    plot_confusion_matrix(y_test.argmax(axis = 1), predictions.argmax(axis = 1), classes=encoder.classes_)
+    plt.show()
+    return m #returns best performing model
+
+
+def model_1():
+    model = Sequential()
+    model.add(Embedding(input_dim = (len(tokenizer.word_counts) + 1), output_dim = 128, input_length = MAX_SEQ_LEN))
+    model.add(LSTM(128))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+m1 = train(model_1,
+           train_text_vec,
+           y_train,
+           test_text_vec,
+           y_test,
+           val_text_vec,
+           y_val,
+           checkpoint_path='model_1.h5',
+           class_weights=cws_dict
+          )
